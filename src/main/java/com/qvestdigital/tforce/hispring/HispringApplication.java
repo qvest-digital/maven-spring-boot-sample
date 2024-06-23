d46d2c797516634478795ba62c8fa038dc10cab3
mirabilos <t.glaser@qvest-digital.com>
2024-06-24 00:43:29 +0200

    as auto-created

---
+++ src/main/java/com/qvestdigital/tforce/hispring/HispringApplication.java
@@ -0,0 +1,12 @@
+package com.qvestdigital.tforce.hispring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HispringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HispringApplication.class, args);
+    }
+}
---
+++ src/test/java/com/qvestdigital/tforce/hispring/HispringApplicationTests.java
@@ -0,0 +1,12 @@
+package com.qvestdigital.tforce.hispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HispringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
